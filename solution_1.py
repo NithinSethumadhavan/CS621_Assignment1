e8a8857db75e7da42c14903feb934a794c90f9c6
Nithin S <tv.nithin.s@gmail.com>
2017-10-17 11:12:43 +0530

    --

--- solution_1.py
+++ solution_1.py
@@ -1,3 +1,8 @@
+# CS 621: Artificial Intelligence
+# Assignment 1, Part 1
+# By Abhijeet Dubey and Nithin S
+# Note: Part 0 taken from file upload by Venkata Naga Hanumath Sai Prasad Kousika
+
 import sys
 variables=['x','y','z','u',]
 constants=['0','a','b','c','d',]
@@ -5,6 +10,7 @@ binary_functions=['+','*',]
 unary_functions=['s','fact',]
 changes={}
 
+# Method defined in Part 0
 def get_middle(string):
 	count=0
 	pos=-1
@@ -21,8 +27,11 @@ def get_middle(string):
 	if (count==0 and pos!=-1):
 		return (pos+1,i+1)
 	else:
-		print ("Error: Misbalance of parameters/parentheses")
+		print ("Invalid Rule: Paranthesis mismatch. Exiting")
 		sys.exit(0)
+
+
+
 
 def compare_tree(left_rule,input_rule):
 	#print ("Compare ",left_rule.string,input_rule.string)
@@ -47,7 +56,7 @@ def compare_tree(left_rule,input_rule):
 	return 0
 
 def replace(rule):
-	global changes	
+	global changes
 	if rule.object_type=='var':
 		if rule.string in changes.keys():
 			return Rule(changes[rule.string])
@@ -91,35 +100,50 @@ def compare_replace(left_rule,right_rule,input_rule):
 	return (0,input_rule)
 
 
-def parse_tree(rules_objects,input_rule):
-	flag=True
-	while flag:
-		flag=False		
-		for rule in rules_objects:
-			if rule.function_type=='->':
-				left_rule=rule.parameter_1
-				right_rule=rule.parameter_2
-				result=compare_replace(left_rule,right_rule,input_rule)
-				if (result[0]):
-					input_rule=result[1]
-					#print(input_rule.string)
-					flag=True
-	return input_rule
+# def parse_tree(rules_objects,input_rule):
+# 	flag=True
+# 	while flag:
+# 		flag=False
+# 		for rule in rules_objects:
+# 			if rule.function_type=='->':
+# 				left_rule=rule.parameter_1
+# 				right_rule=rule.parameter_2
+# 				result=compare_replace(left_rule,right_rule,input_rule)
+# 				if (result[0]):
+# 					input_rule=result[1]
+# 					#print(input_rule.string)
+# 					flag=True
+# 	return input_rule
 
 
+
+
+def evaluate(rules,term):
+	for rule in rules:
+		if rule.function_type=='->':
+			left_rule=rule.parameter_1
+			right_rule=rule.parameter_2
+			result=compare_replace(left_rule,right_rule,term)
+			if (result[0]):
+				return evaluate(rules,result[1])
+	return term
+
+
+
+
+
+
+# Class defined in  Part 0
 class Rule:
 	object_type=''
 	function_type=''
 	parameter_1=''
 	parameter_2=''
 	def explore(self,rule):
-		name_pos=0
-		for each in rule:
-			if (each=='('):
-				break
-			name_pos+=1
-		name=rule[:name_pos]
-		#print(name)
+		first_par = rule.find('(')
+		if(first_par == -1):
+			 first_par = len(rule)
+		name=rule[:first_par]
 		if '->' in rule:
 			self.object_type='bfun'
 			self.function_type='->'
@@ -133,9 +157,9 @@ class Rule:
 		elif name in binary_functions:
 			self.object_type='bfun'
 			self.function_type=name
-			split_index=get_middle(rule[name_pos:])
+			split_index=get_middle(rule[first_par:])
 			try:
-				self.parameter_1=Rule(rule[name_pos+1:split_index[0]])
+				self.parameter_1=Rule(rule[first_par+1:split_index[0]])
 				self.parameter_2=Rule(rule[split_index[0]+1:split_index[1]])
 			except Exception as error:
 				print ("Error: ",error)
@@ -143,22 +167,44 @@ class Rule:
 		elif name in unary_functions:
 			self.object_type='ufun'
 			self.function_type=name
-			if rule[name_pos]!='(' or rule[-1]!=')':
+			if rule[first_par]!='(' or rule[-1]!=')':
 				print ("Error: Misbalance of parameters/parentheses")
 				sys.exit(0)
 			try:
-				self.parameter_1=Rule(rule[name_pos+1:-1])
+				self.parameter_1=Rule(rule[first_par+1:-1])
 				self.parameter_2=""
 			except Exception  as error:
 				print ("Error: ",error)
-				sys.exit()	
+				sys.exit()
 		else:
-			print ("Error: '"+name+"' not found")	
+			print ("Error: '"+name+"' not found")
 			sys.exit(0)
 	def __init__(self,string):
 		self.string=string
 		self.explore(self.string)
-	
+
+	def traverse(self):
+		print ("|Type", self.object_type)
+		if (self.object_type=='bfun'):
+			print ("|\t"+self.string)
+			print ("|\tFunction:  ",self.function_type)
+			print ("|\tParameter1:",self.parameter_1.string)
+			print ("|\tParameter2:",self.parameter_2.string)
+			if (self.parameter_1!=""):
+				self.parameter_1.traverse()
+			if (self.parameter_2!=""):
+				self.parameter_2.traverse()
+		elif (self.object_type=='ufun'):
+			print ("|\t"+self.string)
+			print ("|\tFunction: ",self.function_type)
+			print ("|\tParameter:",self.parameter_1.string)
+			if (self.parameter_1!=""):
+				self.parameter_1.traverse()
+		else:
+			print ("|\tValue: ", self.string)
+
+
+# Method defined in Part 0
 
 def read_rules():
 	file=open("rules.txt","r")
@@ -166,6 +212,7 @@ def read_rules():
 	file.close()
 	rules_objects=[]
 	for each in rules:
+		each=each.replace("\n","")
 		each=each.replace(" ","")
 		print(each)
 		if (each):
@@ -174,10 +221,10 @@ def read_rules():
 
 	input_rule=input("Enter Term: ")
 	input_rule_object=Rule(input_rule)
-	result_rule=parse_tree(rules_objects,input_rule_object)
+	result_rule=evaluate(rules_objects,input_rule_object)
 	print ("Result: ",result_rule.string)
 
 if __name__ == "__main__":
     read_rules()
-    
+
 ### Done
